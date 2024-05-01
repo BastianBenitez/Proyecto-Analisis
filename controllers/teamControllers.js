e069cab53c2ac0d521d1e2eb81aea01cfb1d84a1
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2024-05-01 11:01:59 -0400

    feat: :sparkles: Retrieve Teams Belonging to a Specific User
    Añadida función getMyTeams para recuperar los equipos asociados a un usuario específico, construyendo dinámicamente la consulta SQL en función de los IDs de equipos obtenidos de la base de datos.

--- controllers/teamControllers.js
+++ controllers/teamControllers.js
@@ -1,6 +1,6 @@
 import connection from '../config/connectionDB.js';
 
-const getDetailMyTeam = async (req, res) =>{
+const getDetailMyTeam = async (req, res) => {
     const teamID = req.params.id;
     const query = 'SELECT * FROM equipos WHERE EquipoID = ?';
 
@@ -20,5 +20,33 @@ const getDetailMyTeam = async (req, res) =>{
     }
 };
 
+const getMyTeams = async (req, res) => {
+    const userID = req.params.id;
+    const queryUser = 'SELECT EquipoID FROM miembros_equipo WHERE UsuarioID = ?';
+    const queryTeam = 'SELECT * FROM equipos WHERE ';
 
-export default { getDetailMyTeam }
+    try {
+        const [userResult] = await connection.query(queryUser, [userID]);
+        const equiposIDs = userResult.map(row => row.EquipoID);
+
+        if (equiposIDs.length === 0) {
+            res.status(404).send("El usuario no está asociado a ningún equipo.");
+            return;
+        }
+
+        let condiciones = equiposIDs.map(id => `EquipoID = ${id}`).join(' OR ');
+        const consultaCompleta = queryTeam + condiciones;
+
+        const [teamResult] = await connection.query(consultaCompleta);
+        console.log(teamResult)
+        res.send(teamResult);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error al obtener los equipos del usuario.");
+    }
+};
+
+
+
+export default { getMyTeams, getDetailMyTeam }
