6b4074267f3a162e8bc10aac9c804828434bb397
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2024-05-05 06:29:43 -0400

    feat: Creacion de script para recuperar datos
    Se creo un funcion de recuperacion de datos para obtener los datos del toneo que se este creando.

---
+++ public/js/recoverDatesTournament.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById('tournamentname').value;
+    const description = document.getElementById('description').value;
+    const dateStart = document.getElementById('datestart').value;
+    const dateFinish = document.getElementById('datefinish').value;
+    console.log(name)
+    console.log(description)
+    console.log(dateStart)
+    console.log(dateFinish)
+
+
+    try {
+        const response = await fetch('/tournament/newtournament', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, description, dateStart, dateFinish })
+        });
+
+        const data = await response.json();
+
+        if (data.redirect){
+        window.location.href = data.redirect;
+        }
+
+        if (data.success) {
+        document.getElementById("alert").className = "correct";
+        document.getElementById("alert").innerText = "Registro exitoso";
+        } else {
+        document.getElementById("alert").className = "error";
+        document.getElementById("alert").innerText = data.message || 'Error en el registro';
+        return;
+        }
+    } catch (error) {
+        console.error('Error al registrar:', error);
+        document.getElementById("alert").className = "error";
+        document.getElementById("alert").innerText = "Error al registrar, por favor intenta de nuevo";
+    }
+    });
+});
